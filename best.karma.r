45a0f71d15326b3058357e2542457e6916712fb8
tatianefribeiro <70333564+tatianefribeiro@users.noreply.github.com>
2023-10-08 11:00:05 -0300

    best.karma.r

--- best.karma.r
+++ best.karma.r
@@ -1,31 +1,30 @@
-# Função que testa todos os possiveis modelos de uma KARMA e retorna o melhor modelo
-# segundo o criterio de AIC.
+# Function to test various BARMA models and returns the best one
+# according to AIC criterion
 #
-# Implementado por Fabio M Bayer (bayer@ufsm.br) em 02/10/2014
+# Implemented by Fabio M Bayer (bayer@ufsm.br) on October 02, 2014 
 #
-# Alterado por Renata R Guerra (renata.r.guerra@ufsm.br) em 13/11/2018
-# Dados de entrada:
-# - serie: e a serie temporal de interesse;
-# - sf:  informacao do inicio e da frequencia da serie temporal que deve estar da seguinte maneira.
-#     ex: sf<-c(start=c(1994,7),frequency=12)  # informa o inecio e a frequencia da serie temporal
-# - h: quantidade de observacoes que serao reservadas para fazer previsoes dentre deste intervalo da amostra.
-# - pmax, qmax, Pmax, Qmax: ordens maximas a serem testadas no modelo SARIMAX. Se nao informar valores serao assumidos os padroes vistos abaixo.
-
+# Changed by Renata R Guerra (renata.r.guerra@ufsm.br) on November 13, 2018
+# Data of input:
+# - time series: it is the time series of interest
+# - sf:  information about the start and frequency of the time series should follow the following format:
+#     ex: sf<-c(start=c(1994,7),frequency=12)  # it informs the start and the frequency of the time series
+# - h: number of observations that will be separated from the sample for out-of-sample forecasting
+# - pmax, qmax, Pmax, Qmax: maximum orders to be tested. If these values were not informed, the default values below will be used
+#
 # OBS.: 
-#  1- As previsoes para as variaveis explicativas sao feitas com Hol-Winters.
-#  2- Essa usa modelos aninhados
-
+#  1- The forecasts for the regressors are obtained from Holt-Winters.
+#  2- It uses nested models 
 
 best.karma<-function(serie, sf, h=6, pmax=6, qmax=6, nbest=10,link = "logit",X=NA,X_hat=NA)
 {
   source("karma.r")
   #n<-length(serie)
   #out<-seq((n-h+1),n,1)  # para retirar as ultimas h observacoes
-  #y<-ts(serie[-out],start=c(sf[1],sf[2]),frequency=sf[3]) # serie sem as ultimas h observacoes
+  #y<-ts(serie[-out],start=c(sf[1],sf[2]),frequency=sf[3]) # time series without the last observations
   y<-ts(serie,start=c(sf[1],sf[2]),frequency=sf[3])
   
   
-  # inicializa os criterios AIC
+  # It initializes the AIC criteria
   fit<-karma(y, ar=1, diag=0,link = link)
   aicmin<-fit$aic
   
@@ -34,13 +33,13 @@ best.karma<-function(serie, sf, h=6, pmax=6, qmax=6, nbest=10,link = "logit",X=N
   model1<-model2<-model3<-model4<-model5<-0
   model6<-model7<-model8<-model8<-model10<-0
   
-  best_aic<-rep(Inf,nbest) # guarda os 10 menores AICs
-  #melhores<-rep(0,(nbest)) # guarda as ordem dos 10 melhores modelos
-  melhores<-matrix(rep(0,(nbest*3)),ncol=3) # guarda as ordem dos 10 melhores modelos
+  best_aic<-rep(Inf,nbest) # It saves the 10 smallest AICs
+  #melhores<-rep(0,(nbest)) # It saves the order of the 10 best models
+  melhores<-matrix(rep(0,(nbest*3)),ncol=3) # It saves the order of the 10 best models
   colnames(melhores)<-c("p","q","AIC")
   
-  tot<-0 # inicializa contador de quantos modelos serao testados
-  pau<-0 # incializa contador de quantas vezes deu pau na estimacao Arima(...)
+  tot<-0 # It initializes the count of how many models will be tested.
+  bug<-0 # Initializes a counter for how many times the Arima(...) estimation failed
   
   for(p in 0:pmax)
   { 
@@ -58,10 +57,10 @@ best.karma<-function(serie, sf, h=6, pmax=6, qmax=6, nbest=10,link = "logit",X=N
         if(fitkarma$conv != 0)
         {  
           print(c("NO CONVERGENCE  ",p,q),quote=F)
-          pau<-pau+1
-          next # sai desse loop e vai para o proximo
+          bug<-bug+1
+          next # Exit this loop and move to the next one
         }          
-        if(aicmin>fitkarma$aic) # melhor modelo segundo o AIC
+        if(aicmin>fitkarma$aic) # best model according to AIC
         {  
           aicmin<-fitkarma$aic
           best_model_aic <- fitkarma$model
@@ -82,16 +81,15 @@ best.karma<-function(serie, sf, h=6, pmax=6, qmax=6, nbest=10,link = "logit",X=N
   }
   
   
-  
   print(" ",quote=F)
-  print("MODELO SELECIONADO VIA AIC",quote=F)
+  print("SELECTED MODEL FROM AIC",quote=F)
   print(best_model_aic,quote=F)
   print(" ",quote=F)
-  print(c("Total de modelos testados =",tot),quote=F)
-  print(c("Total erros na estimacao =",pau),quote=F)
+  print(c("Total of tested models =",tot),quote=F)
+  print(c("Total of errors in the estimation =",bug),quote=F)
   
   print(" ",quote=F)
-  print("OS MELHORES MODELOS",quote=F)
+  print("THE BEST MODELS",quote=F)
   print(melhores,quote=F)
   
 }
