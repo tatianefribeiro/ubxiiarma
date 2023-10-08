462127f9a92d3c64101549d7e4e3da0cc914dd78
tatianefribeiro <70333564+tatianefribeiro@users.noreply.github.com>
2023-10-08 11:03:06 -0300

    ubxii-funcs.r

--- ubxii-funcs.r
+++ ubxii-funcs.r
@@ -1,15 +1,15 @@
 # cumulative distribution function
-cdf_UBXII <- function(y,q,c,tau=0.5)   #UBXII cdf (useful for qqplot)
+cdf_UBXII <- function(y,q,c,tau=0.5)    
 {
   (1+(log(1/y))^c)^(log(tau)/log(1+(log(1/q))^c))
 }
 
 
-# inversion method for randon generation
-r_UBXII <- function(n,q_t,c,tau=0.5)   #It generates occurences of Y_i ~ UBXII (q_t, c)
+# inversion method to generate samples from UBXII distribution (Y_t ~ UBXII (q_t, c))
+r_UBXII <- function(n,q_t,c,tau=0.5)   
 {
   u = runif(n)
-  y_UBXII = exp(-(u^(-1/(log(1/tau)/log(1+(log(1/q_t))^c)))-1)^(1/c)) #UBXII qf
+  y_UBXII = exp(-(u^(-1/(log(1/tau)/log(1+(log(1/q_t))^c)))-1)^(1/c)) # UBXII qf
   return(y_UBXII)
 }
 
